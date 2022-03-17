d13a006d9e348dd4f341cf084a0a3c009958e0b6
Javier Arranz <javier.arranz@mirai-advisory.com>
2022-03-17 18:08:07 +0100

    update aws provider and local sensitive fix for TF

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "local_file" "public_key_openssh" {
 resource "local_file" "private_key_pem" {
   count             = local.enabled && var.generate_ssh_key == true ? 1 : 0
   depends_on        = [tls_private_key.default]
-  sensitive_content = tls_private_key.default[0].private_key_pem
+  local_sensitive_file = tls_private_key.default[0].private_key_pem
   filename          = local.private_key_filename
   file_permission   = "0600"
 }
